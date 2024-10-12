b092195f89fff6a317dc4b78ced55b4688f90b9f
gopal0430 <gopalakrishnan0430@gmail.com>
2024-10-12 17:06:53 +0530

    first commit

---
+++ www.maharajarice.com/wp-content/themes/babystreet-child/js/babystreet-front.js
@@ -0,0 +1,18 @@
+(function($) {
+    "use strict";
+
+    /**
+     * Use this file to override javascript functions
+     * defined in the Babystreet parent theme babystreet-front.js file
+     *
+     * For example:
+     *
+     *  window.babystreetStickyHeaderInit = function (){
+     *    // Your code here will be executed instead of
+     *    // those from the parent theme's function
+     * }
+     *
+     */
+
+})(window.jQuery);
+
